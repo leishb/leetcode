c7e450dc62649df9079f5b4c11739bd39340a6ad
leishengbao <leishengbao@qiyi.com>
2017-09-18 11:13:47 +0800

    addBinary

--- src/main/java/com/leishb/leetcode/math/AddBinary.java
+++ src/main/java/com/leishb/leetcode/math/AddBinary.java
@@ -1,11 +1,4 @@
-/*
- * *****************************************************
- * Copyright (C) 2016 iQIYI.COM - All Rights Reserved
- * This file is part of iQiyi Pay project.
- * Unauthorized copy of this file, via any medium is strictly prohibited.
- * Proprietary and Confidential.
- * ****************************************************
- */
+
 package com.leishb.leetcode.math;
 
 /**
