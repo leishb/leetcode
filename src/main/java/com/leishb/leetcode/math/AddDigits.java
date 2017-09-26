e6a8d9780372404acd82302b6bd2bbd4d3ad9f8d
fireflowers <394898568@qq.com>
2017-09-26 10:00:06 +0800

    addDigit

--- src/main/java/com/leishb/leetcode/math/AddDigits.java
+++ src/main/java/com/leishb/leetcode/math/AddDigits.java
@@ -4,4 +4,18 @@ package com.leishb.leetcode.math;
  * Created by me on 2017/9/26.
  */
 public class AddDigits {
+
+
+
+    public int addDigit(int num){
+        while (num <=10){
+            int sum=0;
+            while (num!=0){
+                sum+=num%10;
+                num/=10;
+            }
+            num=sum;
+        }
+        return num;
+    }
 }
--- src/main/java/com/leishb/leetcode/math/CountDigitOne.java
+++ src/main/java/com/leishb/leetcode/math/CountDigitOne.java
@@ -1,7 +1,36 @@
 package com.leishb.leetcode.math;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 /**
  * Created by me on 2017/9/25.
  */
 public class CountDigitOne {
+
+    @Test
+    public void test(){
+//        Assert.assertTrue(countDigitOne(9)==1);
+        Assert.assertTrue(countDigitOne(10)==2);
+    }
+
+
+    public int countDigitOne(int n){
+        int count = 0;
+        int iFactor = 1;
+        while (iFactor <= n){
+            int mode = (n/iFactor)%10;
+            if (mode==0){
+                count += n/(iFactor*10) * iFactor;
+            }
+            if (mode==1){
+                count += n/(iFactor*10) * iFactor + n%(iFactor*10);
+            }
+            if (mode >1){
+                count += n/(iFactor*10) * iFactor +  iFactor;
+            }
+            iFactor *= 10;
+        }
+        return count;
+    }
 }
