044e7458b7f2a77bd2d163be964eca67611ee469
fireflowers <394898568@qq.com>
2017-12-01 20:56:50 +0800

    sliding window

---
+++ src/main/java/com/leishb/leetcode/tag/SlidingWindow.java
@@ -0,0 +1,12 @@
+package com.leishb.leetcode.tag;
+
+import java.lang.annotation.*;
+
+/**
+ * Created by me on 2017/12/1.
+ */
+@Target({ElementType.TYPE,ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface SlidingWindow {
+}
